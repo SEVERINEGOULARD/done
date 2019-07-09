0fc73095dfd2dfa227fae4994777c9be4dfb85ad
Audrey <audreyllavador@hotmail.fr>
2019-07-09 16:30:32 +0200

    Register/Admin view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,12 +10,12 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
-
+                <!--Pseudo-->
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="pseudo" class="col-md-4 col-form-label text-md-right">{{ __('Pseudo') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="pseudo" type="text" class="form-control @error('name') is-invalid @enderror" name="pseudo" value="{{ old('pseudo') }}" required autocomplete="pseudo" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -24,9 +24,9 @@
                                 @enderror
                             </div>
                         </div>
-
+                <!--Email-->
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Adresse Email') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -39,8 +39,9 @@
                             </div>
                         </div>
 
+                <!--Password-->
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Mot de passe') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -53,18 +54,72 @@
                             </div>
                         </div>
 
+                <!--Password Confirm-->
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmation du mot de passe') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
+                <!--City-->
+                <!-- Must be set on "ville" regarding the API-->
+                        <div class="form-group row">
+                            <label for="ville" class="col-md-4 col-form-label text-md-right">{{ __('Ville') }}</label>
+
+                            <div class="col-md-6">
+                                
+                                <input id="ville" type="text" class="form-control" name="ville" value="{{ old('ville') }}" required>
+                               
+                                   <ul style="position:absolute; z-index:100;">
+                                      <li data-vicopo="#ville">
+                                        <strong data-vicopo-code-postal></strong>
+                                        <span data-vicopo-ville></span>
+                                      </li>
+                                   </ul>
+                            </div>
+                        </div>
+                <!--Avatar-->
+                 
+                         <div class="form-group">
+                            <label for="exampleFormControlFile1">Avatar</label>
+                            <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                         </div>
+
+                <!--Theme-->
+                        <div class="row">
+                            <label for="theme" class="col-md-4 col-form-label text-md-right">{{ __('Theme') }}</label>
+                        </div>
+
+                        <div class="row">
+                            <img src="img/theme1/theme1.jpg" style="width:30%;">
+                            <img src="img/theme2/theme2.jpg" style="width:30%;">
+                            <img src="img/theme3/theme3.jpg" style="width:30%;">
+                        </div>
+
+                        <div class="row">
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                                <label class="form-check-label" for="inlineRadio1">Classic</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                                <label class="form-check-label" for="inlineRadio2">Nature</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3">
+                                <label class="form-check-label" for="inlineRadio3">Espace</label>
+                            </div>
+                        </div>
+
+
+
+                <!--Sumit Button-->
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    {{ __('Valider') }}
                                 </button>
                             </div>
                         </div>
@@ -74,4 +129,6 @@
         </div>
     </div>
 </div>
+
+<script src="js/api/villes.js"></script>
 @endsection
