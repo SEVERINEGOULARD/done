ed959e41301f8b59136f2afb941d4353f9f9e124
Audrey <audreyllavador@hotmail.fr>
2019-07-18 21:23:45 +0200

    up

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,18 +3,12 @@ $(function(){
 
 
 /*get current week*/
-
     function getWeekNumber(d) {
-        // Copy date so don't modify original
+
         d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
-        // Set to nearest Thursday: current date + 4 - current day number
-        // Make Sunday's day number 7
         d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
-        // Get first day of year
         var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-        // Calculate full weeks to nearest Thursday
         var weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
-        // Return array of year and week number
         return [d.getUTCFullYear(), weekNo];
     }//close getWeekNumber
 
@@ -23,7 +17,7 @@ $(function(){
     document.getElementById("week").value = week;
 
 
-/*Create TextArea*/
+/*Create Module TextArea*/
     function createTextArea(zone, content) {
         zone.append('<form class="textForm" method="post"></form>');
         $textArea = $('<textarea class="cst-textarea" placeholder="Votre texte ici..."></textarea>');
@@ -47,14 +41,15 @@ $(function(){
 
             });
         });
-    }//close creatTextArea
+    }
 
-
+/*Create Module Image*/
     function createImage(zone, base64content) {
         $img = $('<img src="data:image/png;base64,' + base64content + '">');
         zone.append($img);
     }
 
+/*Load saved modules*/
     function createImageEmptyZone(zone) {
         zone.append('<img class="ill-mod-photo" src="img/polaroid.png"><form method="post"  enctype="multipart/form-data"><div class="form-group file-parent"></div></form>');
         $fileInput = $('<input type="file" class="form-control-file">');
